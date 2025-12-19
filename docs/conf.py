7e5ed1c434f8fb7201ff53367dbc28b840f10de9
Stephen Whitlock <s.whitlock@live.com>
2025-12-20 10:50:00 +1100

    doc: remove repo type from config
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,6 @@ html_theme_options = {
     "site_url": "https://constcheck.readthedocs.io/",
     "repo_url": "https://github.com/jshwi/constcheck/",
     "repo_name": "constcheck",
-    "repo_type": "github",
     "edit_uri": "blob/master/docs",
     "globaltoc_collapse": True,
     "features": [
